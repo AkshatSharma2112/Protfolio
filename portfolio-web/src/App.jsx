c58cfb49198b33f23aae72fabdd109e03106dcb7
{Akshat} <{akshatsharma21122002@gmail.com}>
2025-08-31 04:09:51 +0530

    second commit

---
+++ portfolio-web/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Experience from './components/Experience/Experience'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import Skills from './components/Skills/Skills'
+import Work from './components/Work/Work'
+import Education from './components/Education/Education'
+
+function App() {
+  return (
+    <div className="bg-[#050414]">
+      {/* Background Grid */}
+      <div className="absolute inset-0 
+        bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] 
+        bg-[size:14px_24px] 
+        [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
+      </div>
+
+      {/* Main Content */}
+      <div className="relative z-10 pt-20 text-white">
+        <Navbar />
+        <About />
+        <Experience />
+        <Education />
+        <Skills />
+        <Work />
+        <Contact />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ portfolio-web/src/contants.js
@@ -0,0 +1,141 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+
+// Experience Section Logo's
+
+
+// Education Section Logo's
+import ditLogo from './assets/education_logo/dit_logo.png';
+import sriLogo from './assets/education_logo/sri_logo.jpeg';
+import cbsLogo from './assets/education_logo/cbs_logo.png';
+
+// Project Section Logo's
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    // {
+    //   id: ,
+    //   img: ,
+    //   role: "",
+    //   company: "",
+    //   date: "",
+    //   desc: "",
+    //   skills: [
+    //     "",
+        
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: ditLogo,
+      school: "DIT University, Dehradun",
+      date: "2026",
+      grade: "6.83 CGPA",
+      desc: "I am pursuing my B.Tech in Computer Science and Engineering with a specialization in Artificial Intelligence, Machine Learning, and Robotics from DIT University, Dehradun. My academic journey has provided me with a strong foundation in programming, software development, and core computer science principles. I have studied subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Artificial Intelligence, Machine Learning, and Robotics. Along with theoretical knowledge, I have gained hands-on experience through projects, workshops, hackathons, and technical events, which have enhanced my problem-solving, research, and technical skills. My experience at DIT University has been instrumental in shaping my expertise and professional growth in the field of emerging technologies.",
+      degree: "B.Tech in Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: sriLogo,
+      school: "Sri Chaitanya Junior College, Bowrampet, Hyderabad",
+      date: "Apr 2020 - March 2021",
+      grade: "91.4%",
+      desc: "I completed my Class 12 education from Sri Chaitanya Junior College, Bowrampet, Hyderabad, under the Telangana State Board of Intermediate Education (TSBIE), where I studied Physics, Chemistry, Mathematics, and English, with Sanskrit as an additional language.",
+      degree: "TSBIE (XII) - PCM",
+    },
+    {
+      id: 2,
+      img: cbsLogo,
+      school: "Caster Bridge School, Ballia, Uttar Pradesh",
+      date: "Apr 2018 - March 2019",
+      grade: "78.6%",
+      desc: "I completed my Class 10 education from Caster Bridge School, Ballia, Uttar Pradesh, under the CISCE board, where I studied Science with Computer.",
+      degree: "CISCE (X) - Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    // {
+    //   id: 0,
+    //   title: "GitHub Profile Detective",
+    //   description:
+    //     "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+    //   image: githubdetLogo,
+    //   tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    //   github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+    //   webapp: "https://githubprofiledetective.netlify.app/",
+    // },
+    // {
+    //   id: 1,
+    //   title: "CS Prep",
+    //   description:
+    //     "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+    //   image: csprepLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+    //   github: "https://github.com/codingmastr/CSPrep",
+    //   webapp: "https://csprep.netlify.app/",
+    // },
+  ]
